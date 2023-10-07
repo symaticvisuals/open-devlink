a24cddd6cfffe0354bb1f1eb1fecdef945615e50
Deepanshu Goel <symaticvisuals02@gmail.com>
2023-10-07 14:32:41 +0530

    Basic Roting Setup

---
+++ src/app/(pages)/(protected)/home/page.jsx
@@ -0,0 +1,9 @@
+const ProfilePage = () => {
+  return (
+    <div className="container">
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;
--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
    <html lang="en" className='dark'>
       <body>
-        <Providers>
+        <Providers >
           {children}
         </Providers>
       </body>
